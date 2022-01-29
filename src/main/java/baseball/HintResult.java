461c95f30adbf86186986737a7296bff8bb655d5
minho <lights8615@gmail.com>
2022-01-29 15:55:55 +0900

    feat(baseball): 정답 확인

--- src/main/java/baseball/HintResult.java
+++ src/main/java/baseball/HintResult.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class HintResult {
+    private static final int MAX_SIZE = 3;
+
     private final Map<Hint, Integer> hints;
 
     private HintResult(Map<Hint, Integer> hints) {
@@ -27,5 +29,9 @@ public class HintResult {
     @Override
     public int hashCode() {
         return Objects.hash(hints);
+    }
+
+    public boolean isAnswer() {
+        return hints.get(Hint.STRIKE) == MAX_SIZE;
     }
 }
---
+++ src/test/java/baseball/HintResultTest.java
@@ -0,0 +1,43 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class HintResultTest {
+    @ParameterizedTest
+    @CsvSource({"2,0,1",
+        "1,0,2", "1,2,0", "1,1,1",
+        "0,3,0", "0,2,1", "0,1,2", "0,0,3"})
+    void name(int strike, int ball, int nothing) {
+        Map<Hint, Integer> hints = new EnumMap<>(Hint.class);
+        hints.put(Hint.STRIKE, strike);
+        hints.put(Hint.BALL, ball);
+        hints.put(Hint.NOTHING, nothing);
+
+        HintResult hintResult = HintResult.from(hints);
+
+        boolean answer = hintResult.isAnswer();
+
+        assertThat(answer).isFalse();
+    }
+
+    @Test
+    void test() {
+        Map<Hint, Integer> hints = new EnumMap<>(Hint.class);
+        hints.put(Hint.STRIKE, 3);
+        hints.put(Hint.BALL, 0);
+        hints.put(Hint.NOTHING, 0);
+
+        HintResult hintResult = HintResult.from(hints);
+
+        boolean answer = hintResult.isAnswer();
+
+        assertThat(answer).isTrue();
+    }
+}
